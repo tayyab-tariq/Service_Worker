cba24253754e2d8240c9f1180258cccecd42b126
Tayyab Tariq <71798927+tayyab-tariq@users.noreply.github.com>
2023-05-08 17:44:10 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('../cached_site.js')
+        navigator.serviceWorker.register('../cached_site.js', { scope: '/' })
         .then(reg => console.log('Service Worker Registered'))
         .catch(err => console.log(`Service Worker: Error: ${err}`));
     });
